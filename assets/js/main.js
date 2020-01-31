b23c477a6ca813695ee41776c062032dcead7e51
sandikoprastyo <sandikoprastyo@gmail.com>
2020-01-31 17:40:55 +0700

    change hamburger span && adding sroll smooth

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('.third-button').on('click', function () {
+        $('.animated-icon3').toggleClass('open');
+    });
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+
+
+
+
+});
