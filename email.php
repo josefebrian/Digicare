02bb30cec7cb229b0454f9a61dfbf9b2d3234cde
sandikoprastyo <sandikoprastyo@gmail.com>
2020-02-14 02:47:05 +0700

    done

--- email.php
+++ email.php
@@ -1,42 +1,57 @@
-<?
+<?php
+if($_POST){
+	
+    if(empty($_POST['name']) || empty($_POST['phone']) || empty($_POST['email']) || empty($_POST['company']) || empty($_POST['industry']) || empty($_POST['employed'])){
+		echo '<script>
+			$(document).ready(function(){
+				swal("Ops...","harpa diisi ya","warning");
+			});
+			</script>';
+	}else{
+		$name 		= utf8_decode($_POST['name']);
+		$phone 		= utf8_decode($_POST['phone']);
+		$email     	= utf8_decode($_POST['email']);
+        $company 	= utf8_decode($_POST['company']);
+        $industry 	= utf8_decode($_POST['industry']);
+        $employed 	= utf8_decode($_POST['employed']);
+		$assunto 	= 'Kontak dikirim oleh situs web';
+		
+		
+        require_once('assets/plugin/PHPMailer/class.phpmailer.php');
+		$Email = new PHPMailer();
+		$Email->IsSMTP(); 
+		$Email->SMTPAuth = true; 
+		$Email->Host = 'smtp.gmail.com'; 
+		$Email->Port = 587; 
+		$Email->SMTPSecure = 'tls';
+		$Email->Username = 'gunswinger0@gmail.com';
+		$Email->Password = 'Gunungsindur'; 
+		$Email->IsHTML(true); 
+		$Email->From = $_POST["email"];
+        $Email->FromName = $_POST["name"];
+        $Email->addAddress('gunswinger0@gmail.com', 'support@DigiCareId'); 
+        $Email->WordWrap = 50;
+        $Email->IsHTML(true);
+        $Email->Subject =  $_POST["email"];
+        $Email->Body = '<h3>Name : '.$_POST["name"].' <br/> Phone : '.$_POST["phone"].' 
+        <br/> Email : '.$_POST["email"].'  <br/> Company : '.$_POST["company"].'
+        <br/> Industry : '.$_POST["industry"].' <br/> Employed : '.$_POST["employed"].' </h3>';
+		if(!$Email->Send()){				
+			 echo'
+			<script>
+				$(document).ready(function(){
+					swal("Ops..!! '.utf8_encode($name).'...","An error occurred while sending the message, please try again!", "error");
+				});
+			</script>';
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-                
-require 'assets/plugin/PHPMailer/src/PHPMailer.php';
-require 'assets/plugin/PHPMailer/src/Exception.php';
-require 'assets/plugin/PHPMailer/src/SMTP.php';
-list($name, $phone, $email, $company, $industry, $employed) = array_values($_POST);
-$mail = new PHPMailer(true);
-try{
-    $mail->SMTPOptions = array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-        ));
+		}else{
+			 echo'
+		<script>
+			$(document).ready(function(){
+				swal("Success '.utf8_encode($name).'...", "Your message has been sent. Thank you for contacting us!", "success")
+			});
+		</script>';
 
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'gunswinger0@gmail.com';                     // SMTP username
-    $mail->Password   = 'Gunungsindur';                               // SMTP password
-    $mail->SMTPSecure = 'tsl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 587;   
-    $mail->From = $_POST["email"];
-    $mail->FromName = $_POST["name"];
-    $mail->addAddress('gunswinger0@gmail.com', 'support@DigiCareId'); 
-    $mail->WordWrap = 50;
-    $mail->IsHTML(true);
-    $mail->Subject =  $_POST["email"];
-    $mail->Body = '<h3>Name : '.$_POST["name"].' <br/> Phone : '.$_POST["phone"].' 
-    <br/> Email : '.$_POST["email"].'  <br/> Company : '.$_POST["company"].'
-    <br/> Industry : '.$_POST["industry"].' <br/> Employed : '.$_POST["employed"].' </h3>';
-
-        $mail->send();
-        echo "<script>alert('Thank you for contact us');location.href ='https://localhost/Digicare/';</script>";
-        } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
-    
-?>
+		}		
+	}
+}
